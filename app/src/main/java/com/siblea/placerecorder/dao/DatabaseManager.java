34881d48ec91c158ea026ab83c77b41d0425b09e
leonardosibela <leonardo.sibela@dextra-sw.com>
2016-10-13 12:22:40 -0300

    Adding database manager

---
+++ app/src/main/java/com/siblea/placerecorder/dao/DatabaseManager.java
@@ -0,0 +1,33 @@
+package com.siblea.placerecorder.dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseManager extends SQLiteOpenHelper {
+
+    private static final String BASE_NAME = "placerecorder.db";
+    private static final int BASE_VERION = 1;
+
+    public DatabaseManager(Context context) {
+        super(context, BASE_NAME, null, BASE_VERION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String placeTableDDL =
+                "CREATE TABLE place (" +
+                        "id TEXT PRIMARY KEY, " +
+                        "name TEXT NOT NULL, " +
+                        "description TEXT NOT NULL, " +
+                        "lat TEXT, " +
+                        "lng INTEGER)";
+
+        db.execSQL(placeTableDDL);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+}
